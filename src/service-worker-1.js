54070e1c51098a395a3f09fbbd45a08fdc9cbce6
Anastasiia Dzundza <anastasiiadzundza@womply.com>
2020-04-01 20:52:53 +0300

    service worker i js

--- src/service-worker-1.js
+++ src/service-worker-1.js
@@ -1,22 +1,47 @@
-import {registerRoute} from 'workbox-routing';
-import {NetworkFirst} from 'workbox-strategies';
-import {CacheableResponse} from 'workbox-cacheable-response';
+// import {registerRoute} from 'workbox-routing';
+// import {NetworkFirst} from 'workbox-strategies';
+// import {CacheableResponse} from 'workbox-cacheable-response';
+//
+//
+// function cacheResponses() {
+//   registerRoute(
+//     'https://api.citybik.es/v2/networks',
+//     NetworkFirst({
+//       cacheName: 'networks-cache',
+//       plugins: [
+//         new CacheableResponse.Plugin({
+//           statuses: [0, 200],
+//         })
+//       ]
+//     })
+//   );
+// }
+//
+// window.addEventListener('fetch', () => {
+//   cacheResponses();
+// });
 
 
-function cacheResponses() {
-  registerRoute(
-    'https://api.citybik.es/v2/networks',
-    NetworkFirst({
-      cacheName: 'networks-cache',
-      plugins: [
-        new CacheableResponse.Plugin({
-          statuses: [0, 200],
-        })
-      ]
-    })
-  );
-}
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-window.addEventListener('fetch', () => {
-  cacheResponses();
+importScripts(
+  "/precache-manifest.1d855fadf1ca39377c92c99f3278edc2.js"
+);
+
+const cacheableResponse = new workbox.cacheableResponse.Plugin({
+  statuses: [0, 200],
+  headers: {
+    'x-is-cacheable': true,
+  },
 });
+
+// APIs
+workbox.routing.registerRoute(
+  'https://api.citybik.es/v2/networks',
+  workbox.strategies.networkFirst({
+    cacheName: 'networks-cache',
+      plugins: [
+          cacheableResponse
+      ]
+  })
+);
