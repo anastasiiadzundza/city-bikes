75f71dfdf238dcaa504a831bbadb926e5a22067c
Anastasiia <Dzundza>
2020-04-15 15:27:24 +0300

    took away registration of service worker by workbox

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,6 @@ import 'semantic-ui-css/semantic.min.css'
 
 import { render } from 'react-dom';
 import './index.scss';
-import * as serviceWorker from './serviceWorker';
 
 const store = createStore(
     rootReducer,
@@ -24,8 +23,3 @@ render(
     </Provider>,
     document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
